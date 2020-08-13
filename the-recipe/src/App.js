af297365ca3bbd45c2d31a288795c5978ff411a3
Mario90 <mgpinan1990@gmail.com>
2020-08-13 13:28:32 +0100

    Installed react

---
+++ the-recipe/src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Jumbotron, Button, Container } from "reactstrap";
+
+function App() {
+  return (
+    <div className="App">
+      <Container>
+        <div>
+          <Jumbotron>
+            <h1 className="display-3">Hello, world!</h1>
+            <p className="lead">Welcome to the Recipe</p>
+            <hr className="my-2" />
+            <p>Feel free to post any recipe.</p>
+            <p className="lead">
+              <Button color="primary">Learn More</Button>
+            </p>
+          </Jumbotron>
+        </div>
+        Recipe of the day:
+      </Container>
+    </div>
+  );
+}
+
+export default App;
