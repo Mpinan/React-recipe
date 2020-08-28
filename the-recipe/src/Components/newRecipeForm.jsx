3a570bab40ecc9f35981e3dd27f25f28a018c859
Mario90 <mgpinan1990@gmail.com>
2020-08-28 21:11:52 +0100

    Working on dinamic form

--- the-recipe/src/Components/newRecipeForm.jsx
+++ the-recipe/src/Components/newRecipeForm.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, Component } from "react";
 import {
   Col,
   Row,
@@ -21,89 +21,39 @@ class NewRecipeForm extends Component {
     prepTime: "",
     description: "",
     ingredients: "",
-    steps: {},
     stepsCount: 1,
+    steps: [{ id: "", step: "" }],
   };
 
   onChange = (e) => {
     this.setState({ [e.target.name]: e.target.value });
   };
 
-  newRecipe = (e) => {
-    e.preventDefault();
-    console.log(this.state, "----1");
-    fetch(
-      "https://us-central1-the-recipe-api.cloudfunctions.net/app/recipes/create",
-      {
-        method: "post",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          user: {
-            name: this.state.name,
-            serves: this.state.serves,
-            difficulty: this.state.difficulty,
-            type: this.state.type,
-            cookTime: this.state.cookTime,
-            prepTime: this.state.prepTime,
-            description: this.state.description,
-            ingredients: this.state.ingredients,
-            steps: this.state.steps,
-          },
-        }),
-      }
-    )
-      .then((response) => response.json())
-      .catch((err) => {
-        console.error("Error:", err);
-      });
+  // const handleOwnerChange = (e) =>
+  //   setOwnerState({
+  //     ...ownerState,
+  //     [e.target.name]: [e.target.value],
+  //   });
+
+  // const blankCat = { name: "", age: "" };
+  // const [catState, setCatState] = useState([{ ...blankCat }]);
+
+  addStep = () => {
+    let blankStep = [...this.state.steps];
+    this.setState({ steps: blankStep });
   };
 
-  onClickNewStep = () => {
-    this.setState({
-      stepsCount: this.state.stepsCount + 1,
-    });
-
-    let totalSteps = { 0: "" };
-
-    Object.assign(this.state.steps, totalSteps);
-    console.log(totalSteps, "-----");
-  };
-
-  stepObject = () => {
-    let step = {};
-
-    this.setState({});
-  };
-
-  getSteps = () => {
-    console.log(this.state);
-    let steps = [];
-    // this.setState({
-    //   stepsCount: this.state.stepsCount + 1,
-    // });
-    for (let i = 1; i < this.state.stepsCount; i++) {
-      steps.push(
-        <div>
-          <Steps
-            key={i}
-            steps={this.state.steps}
-            count={this.state.stepsCount}
-          />
-          <Button onClick={this.onClickNewStep}>+</Button>
-        </div>
-      );
-    }
-    console.log(steps);
-    return steps;
+  handleStepChange = (e) => {
+    const updatedStep = [...this.state.steps];
+    updatedStep[e.target.dataset.idx][e.target.className] = e.target.value;
+    this.setState({ steps: updatedStep });
   };
 
   render() {
     return (
       <div>
         <Container>
-          <Form onSubmit={this.newRecipe.bind(this)}>
+          <Form>
             <Row form>
               <Col md={3}>
                 <FormGroup>
@@ -199,14 +149,19 @@ class NewRecipeForm extends Component {
               />
             </FormGroup>
             <FormGroup>
-              <Label for="steps">Steps:</Label>
-              <Steps
-                onChange={this.onChange}
-                step={this.state.step}
-                stepsCount={this.state.stepsCount}
+              <input
+                type="button"
+                value="Add New Step"
+                onClick={this.addStep}
               />
-              <Button onClick={this.onClickNewStep}>+</Button>
-              {this.getSteps()}
+              {this.state.steps.map((val, idx) => (
+                <Steps
+                  key={`step-${idx}`}
+                  idx={idx}
+                  steps={this.state.steps}
+                  handleStepChange={this.handleStepChange}
+                />
+              ))}
             </FormGroup>
             <Button>Post Recipe</Button>
           </Form>
@@ -217,3 +172,197 @@ class NewRecipeForm extends Component {
 }
 
 export default NewRecipeForm;
+
+// class NewRecipeForm extends Component {
+//   state = {
+//     name: "",
+//     serves: 0,
+//     difficulty: "",
+//     type: "",
+//     cookTime: "",
+//     prepTime: "",
+//     description: "",
+//     ingredients: "",
+//     stepsCount: 1,
+//     steps: [{ id: "", step: "" }],
+//   };
+
+//   onChange = (e) => {
+//     this.setState({ [e.target.name]: e.target.value });
+//   };
+
+//   newRecipe = (e) => {
+//     e.preventDefault();
+//     console.log(this.state, "----1");
+//     fetch(
+//       "https://us-central1-the-recipe-api.cloudfunctions.net/app/recipes/create",
+//       {
+//         method: "post",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify({
+//           user: {
+//             name: this.state.name,
+//             serves: this.state.serves,
+//             difficulty: this.state.difficulty,
+//             type: this.state.type,
+//             cookTime: this.state.cookTime,
+//             prepTime: this.state.prepTime,
+//             description: this.state.description,
+//             ingredients: this.state.ingredients,
+//             steps: this.state.steps,
+//           },
+//         }),
+//       }
+//     )
+//       .then((response) => response.json())
+//       .catch((err) => {
+//         console.error("Error:", err);
+//       });
+//   };
+
+//   onClickNewStep = () => {
+//     this.setState({
+//       stepsCount: this.state.stepsCount + 1,
+//     });
+
+//     let totalSteps = { 0: "" };
+
+//     console.log(totalSteps, "-----");
+//   };
+
+//   getSteps = () => {
+//     console.log(this.state);
+//     let steps = [...this.state.steps];
+
+//     this.state.steps.map((id, des) => {
+//       steps.push(
+//         <div key={id}>
+//           <Steps key={id} step={des} />
+//           <Button onClick={this.onClickNewStep}>+</Button>
+//         </div>
+//       );
+//     });
+//     console.log(steps);
+//     return steps;
+//   };
+
+//   render() {
+//     return (
+//       <div>
+//         <Container>
+//           <Form onSubmit={this.newRecipe.bind(this)}>
+//             <Row form>
+//               <Col md={3}>
+//                 <FormGroup>
+//                   <Label for="name">Name</Label>
+//                   <Input
+//                     type="text"
+//                     name="name"
+//                     placeholder="Name of dish"
+//                     id="name"
+//                     onChange={this.onChange}
+//                   />
+//                 </FormGroup>
+//               </Col>
+//               <Col md={3}>
+//                 <FormGroup>
+//                   <Label for="serves">Serves</Label>
+//                   <Input
+//                     type="text"
+//                     name="serves"
+//                     id="serves"
+//                     placeholder="1, 2, 3, 4..."
+//                     onChange={this.onChange}
+//                   />
+//                 </FormGroup>
+//               </Col>
+//               <Col md={3}>
+//                 <FormGroup>
+//                   <Label for="difficulty">Difficulty</Label>
+//                   <Input
+//                     type="text"
+//                     name="difficulty"
+//                     id="difficulty"
+//                     placeholder="Easy, Medium, Hard"
+//                     onChange={this.onChange}
+//                   />
+//                 </FormGroup>
+//               </Col>
+//               <Col md={3}>
+//                 <FormGroup>
+//                   <Label for="type">Type</Label>
+//                   <Input
+//                     type="text"
+//                     name="type"
+//                     id="type"
+//                     placeholder="Main, Dessert or Drink"
+//                     onChange={this.onChange}
+//                   />
+//                 </FormGroup>
+//               </Col>
+//               <Col md={3}>
+//                 <FormGroup>
+//                   <Label for="cookTime">Cooking Time</Label>
+//                   <Input
+//                     type="text"
+//                     name="cookTime"
+//                     id="cookTime"
+//                     placeholder="Minutes"
+//                     onChange={this.onChange}
+//                   />
+//                 </FormGroup>
+//               </Col>
+//               <Col md={3}>
+//                 <FormGroup>
+//                   <Label for="prepTime">Preparation Time</Label>
+//                   <Input
+//                     type="text"
+//                     name="prepTime"
+//                     id="prepTime"
+//                     placeholder="Minutes"
+//                     onChange={this.onChange}
+//                   />
+//                 </FormGroup>
+//               </Col>
+//             </Row>
+//             <FormGroup>
+//               <Label for="description">Description</Label>
+//               <Input
+//                 type="text"
+//                 name="description"
+//                 id="description"
+//                 placeholder="Short description of your dish"
+//                 onChange={this.onChange}
+//               />
+//             </FormGroup>
+//             <FormGroup>
+//               <Label for="ingredients">Ingredients</Label>
+//               <Input
+//                 type="text"
+//                 name="ingredients"
+//                 id="ingredients"
+//                 placeholder="1 tbs oil, 4g of salt... etc"
+//                 onChange={this.onChange}
+//               />
+//             </FormGroup>
+//             <FormGroup>
+//               <Label for="steps">Steps:</Label>
+//               <Steps
+//                 onChange={this.onChange}
+//                 step={this.state.step}
+//                 stepsCount={this.state.stepsCount}
+//               />
+//               <Button onClick={this.onClickNewStep}>+</Button>
+//               {this.getSteps()}
+//             </FormGroup>
+//             <Button>Post Recipe</Button>
+//           </Form>
+//         </Container>
+//       </div>
+//     );
+//   }
+// }
+
+// export default NewRecipeForm;
--- the-recipe/src/Components/steps.jsx
+++ the-recipe/src/Components/steps.jsx
@@ -1,40 +1,22 @@
 import React, { Component } from "react";
 import { Col, FormGroup, Label, Input } from "reactstrap";
 
-class Steps extends Component {
-  state = {
-    step: "",
-  };
-
-  onChange = (e) => {
-    this.setState({ [e.target.name]: e.target.value });
-    console.log(this.state.step);
-  };
-
-  addToStep = () => {
-    let count = 0;
-    let step = { "1": this.state.step };
-    // Object.assign(this.props.steps, { 1: this.state.step });
-    let object = {};
-    Object.assign(object, step);
-    // console.log(step);
-    console.log(object);
-  };
-
-  render() {
-    return (
-      <div>
-        <Input
-          type="text"
-          name="step"
-          id="exampleText"
-          placeholder="First add oil to the pan...."
-          onChange={this.onChange}
-        />
-        {this.addToStep()}
-      </div>
-    );
-  }
-}
+const Steps = ({ idx, steps, handleStepChange }) => {
+  const description = `description-${idx}`;
+  return (
+    <div key={`cat-${idx}`}>
+      <Label htmlFor={description}>{`Step ${idx + 1}`} Description</Label>
+      <Input
+        type="text"
+        name={description}
+        data-idx={idx}
+        id={description}
+        className="description"
+        value={steps[idx].description}
+        onChange={handleStepChange}
+      />
+    </div>
+  );
+};
 
 export default Steps;
